c3c29380733f590ea9201e458544f8c20833d2ff
jsdev305 <jsdev305@gmail.com>
2026-01-14 18:56:11 -0500

    added logger

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
-
+import { logger } from "hono/logger";
 const app = new OpenAPIHono();
+
+app.use(logger());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
 app.notFound((c) => {
-    return c.text("Page does not exist - 404", 404)
-})
+  return c.text("Page does not exist - 404", 404);
+});
 
 export default app;
