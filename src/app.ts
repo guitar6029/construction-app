f38db58c3a77c31479a1b6058c8ae1fe68b0d729
jsdev305 <jsdev305@gmail.com>
2026-01-15 18:55:50 -0500

    added swagger ui

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { OpenAPIHono } from "@hono/zod-openapi";
 import { logger } from "hono/logger";
 import HealthRouter from "./routes/health.routes";
 import ProjectsRouter from "./routes/projects.routes";
+import { swaggerUI } from "@hono/swagger-ui";
 const app = new OpenAPIHono();
 
 app.use(logger());
@@ -13,16 +14,20 @@ app.get("/", (c) => {
 app.route("/health", HealthRouter);
 app.route("/projects", ProjectsRouter);
 
-app.notFound((c) => {
-  return c.text("Page does not exist - 404", 404);
-});
+// Swagger UI
+app.get("/docs", swaggerUI({ url: "/doc" }));
 
+// openAPI JSON
 app.doc("/doc", {
   openapi: "3.0.0",
   info: {
     version: "1.0.0",
-    title: "My API",
+    title: "Construction API",
   },
+});
+
+app.notFound((c) => {
+  return c.text("Page does not exist - 404", 404);
 });
 
 export default app;
